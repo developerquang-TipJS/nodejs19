b4622a169a81d6fd79ca38a63d12eb069405c320
QuangDeveloper <ng.quang.coder.new@gmail.com>
2026-09-05 23:37:43 +0700

    build-base

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const compression = require('compression')
+const app = express()
+
+// init middlewares
+app.use(morgan("dev"))
+app.use(helmet())
+app.use(compression())
+// init routers
+app.get("/", (req, res, next) => {
+    const str = "hello quang"
+    return res.status(200).json({
+        message: 'Welcome home page',
+        metadata: str.repeat(1000000)
+    })
+})
+module.exports = app
