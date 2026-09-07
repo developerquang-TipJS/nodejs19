d39ef0be67e4745f8369cc97a21c4436354020eb
QuangDeveloper <ng.quang.coder.new@gmail.com>
2026-09-07 23:00:58 +0700

    build-router-controller-service-model-flow-base

--- src/app.js
+++ src/app.js
@@ -8,14 +8,11 @@ const app = express()
 app.use(morgan("dev"))
 app.use(helmet())
 app.use(compression())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 // init db
 require('./dbs/init.mongodb')
 // init routers
-app.get("/", (req, res, next) => {
-    const str = "hello quang"
-    return res.status(200).json({
-        message: 'Welcome home page',
-        metadata: str.repeat(1000000)
-    })
-})
+app.use('', require('./routes'))
+
 module.exports = app
---
+++ src/services/access.service.js
@@ -0,0 +1,9 @@
+'use strict'
+
+class AccessService {
+    static signUp = async () => {
+
+    }
+}
+
+module.exports = AccessService
