ce48b1c5c99a9d4b4a9c9399c7e2321a0e79296e
BMBSOFT\quang.nguyen2 <quang.nguyen2@bmbsoft.com.vn>
2026-09-09 11:54:12 +0700

    handle-create-response-custom

---
+++ src/core/success.response.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const StatusCodes = {
+    OK : 200,
+    CREATED : 201
+}
+
+const ReasonStatusCodes = {
+    OK : 200,
+    CREATED : 201
+}
+class SuccessResponse {
+    constructor({message, statusCode = StatusCodes.OK, reasonStatusCode = ReasonStatusCodes.OK, metadata = {}}) {
+        this.message = !message ? reasonStatusCode : message
+        this.status = statusCode
+        this.metadata = metadata
+    }
+
+    send(res,headers = {}) {
+        return res.status(this.status).json(this)
+    }
+}
+
+class OK extends SuccessResponse{
+    constructor({message,metadata}) {
+        super({message,metadata})
+    }
+}
+
+class CREATED extends SuccessResponse{
+    constructor({message,statusCode = StatusCodes.CREATED, reasonStatusCode = ReasonStatusCodes.CREATED,metadata}) {
+        super({message,statusCode,reasonStatusCode,metadata})
+    }
+}
+
+module.exports = {
+    OK,
+    CREATED
+}
